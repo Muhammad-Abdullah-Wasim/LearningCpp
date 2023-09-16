720dcfd27d9371cc591fb7e40d1784f06c49d899
Muhammad Abdullah <maw180604@gmail.com>
2023-09-16 17:23:49 +0500

    added Data Types to the learning repo

---
+++ DataType/datatype.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a;
+    char b;
+    float c;
+    double d;
+    long e;
+    cout << "Size of int is " << sizeof(a) << " bytes" << endl;
+    cout << "Size of char is " << sizeof(b) << " bytes" << endl;
+    cout << "Size of float is " << sizeof(c) << " bytes" << endl;
+    cout << "Size of double is " << sizeof(d) << " bytes" << endl;
+    cout << "Size of long is " << sizeof(e) << " bytes" << endl;
+    return 0;
+}
