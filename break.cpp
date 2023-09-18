c02f9ad8fe3874c9c857b53b25bbf2cc0478b183
Muhammad Abdullah <maw180604@gmail.com>
2023-09-18 17:38:56 +0500

    added break

---
+++ break.cpp
@@ -0,0 +1,25 @@
+/*Certainly! The break and continue statements are control flow statements used in programming to alter the normal flow of control within loops (like for, while, and do-while) and switch statements. They serve different purposes:
+
+break Statement:
+
+The break statement is primarily used to exit or terminate the nearest enclosing loop (either a for, while, or do-while loop) prematurely, without completing the remaining iterations.
+When a break statement is encountered within a loop, the program immediately jumps out of that loop and continues with the code after the loop.
+It is often used when a certain condition is met, and you want to exit the loop early. For example, you might use break to exit a loop when you find a specific item in a list or when a certain error condition occurs.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    while (i <= 10)
+    {
+        if (i == 5)
+        {
+            break;
+        }
+        cout << i << endl;
+        i++;
+    }
+    return 0;
+}
