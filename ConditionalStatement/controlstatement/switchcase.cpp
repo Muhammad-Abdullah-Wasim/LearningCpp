74b4d8b9059cebe05626f2c3a0bd1456f0e1e426
Muhammad Abdullah <maw180604@gmail.com>
2023-09-18 17:28:38 +0500

    updated some errors

--- ConditionalStatement/controlstatement/switchcase.cpp
+++ ConditionalStatement/controlstatement/switchcase.cpp
@@ -1,4 +1,5 @@
 // switch case statement is used instead of conditioanl statement. If we have multiple option to compare an input we can use this.
+// I wrote this code to grade a student based on his/her percentage, But this thing is'nt working because i did'nt knwe that in switch case the value should be constant and we cant put a condition in it.
 #include <stdio.h>
 #include <iostream>
 using namespace std;
@@ -10,7 +11,7 @@ int mian ()
     cin>>percentage;
     switch (percentage)
     {
-    case ( percetnage<=100 && percentage>=85):
+    case (percentage<=100 && percentage>=85):
         cout<<"A+";
         break;
     case (percentage<=84.9 && percentage>= 80):
@@ -21,10 +22,13 @@ int mian ()
         break;
     case (percentage<=69.9 && percentage >= 60):
         cout<<"C";
+        break;
     case (percentage<=59.9 && percentage>= 50):
         cout<<"D";
+        break;
     case (percentage<50):
         cout<<"F";
+        break;
     default:
         cout<<"Input your percentage from 0 to 100";
         break;
