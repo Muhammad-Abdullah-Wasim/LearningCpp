9e2faf3a621e7727d141d30c6dd17a722392220d
Muhammad Abdullah <maw180604@gmail.com>
2023-09-25 16:53:54 +0500

    added function parctice

---
+++ functions/f.cpp
@@ -0,0 +1,11 @@
+/*functions are used to avoid repetetion in code. Lets suppose that we need to find a solution for a problem and we need to use the same code again and again.
+ In this case we can use functions to avoid repetetion of code.*/
+
+//syntax of function is given 
+return_type function_name (argument1, argument2, argument3,....) //parameter/ argument will be with their parametrized data type
+{
+    //body of function
+    //statements
+    //return statement
+}
+//this function will be called in int main function
